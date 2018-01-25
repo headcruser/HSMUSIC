c71fa42b69771b86eb04ca7c6f4db555a65c170b
headcruser <headcruser@gmail.com>
2018-01-24 23:46:42 -0600

    Quitar Aviso de Mongoose Promise  de la consola

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var mongoose=require('mongoose');
 var app = require('./app');
 var port = process.env.PORT || 3977;
 
+mongoose.Promise = global.Promise;
+
 mongoose.connect('mongodb://localhost:27017/hsmusic',(err,res)=>{
     if(err){
         throw err;
