bd78dea492545e6c6614bf9fcc95b4794e610284
headcruser <headcruser@gmail.com>
2018-02-05 14:57:03 -0600

    Preparando el entorno del front-end

---
+++ client/src/app/models/Album.ts
@@ -0,0 +1,10 @@
+export class Artist
+{
+  constructor(
+    public title: string,
+    public description: string,
+    public year: number,
+    public image: string,
+    public artist: string
+  ) {}
+}
---
+++ client/src/app/models/Song.ts
@@ -0,0 +1,10 @@
+export class Song
+{
+  constructor(
+    public number: number,
+    public name: string,
+    public duration: string,
+    public image: string,
+    public album:string
+  ) {}
+}
