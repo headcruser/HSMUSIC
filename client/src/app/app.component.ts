7b5076e51061edf82135985b0f4da99ae37c7557
headcruser <headcruser@gmail.com>
2018-02-05 16:39:17 -0600

    Crear Servicios

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,21 +1,26 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
 import {User} from './models/User';
+import {UserService} from './services/user.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  providers:[UserService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   public title = 'HSMUSIC';
   public user:User;
   public identity;
   public token;
 
-  constructor(){
+  constructor(private _userService:UserService){
     this.user = new User('','','','','','ROLE_USER','');
   }
 
   public onSubmit(){
     console.log(this.user)
+  }
+  ngOnInit(){
+    console.log(this._userService.signup());
   }
 }
