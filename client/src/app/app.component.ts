4bf5496969a0fbb6c5f4d5af14d12be95b5b3242
headcruser <headcruser@gmail.com>
2020-08-09 14:06:42 -0500

    Actualizando proyecto a angular 10
    - Creacion de template (lamentable) desde cero
    -  Instalacion de librerias font (b4,font-awesome)
    - Se Agrego el login principal

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,119 +1,47 @@
-import { Component, OnInit } from '@angular/core'
-import {UserService} from './services/user.service'
-import { User } from 'app/models/User'
-import { GLOBAL } from './services/global'
+import { Component } from '@angular/core';
+import {User} from './models/User';
+import { GLOBAL } from './services/global';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  providers: [UserService]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit
-{
-  //GLOBALS
-  public title = 'HSMUSIC'
-  public user: User
-  public userRegister: User
-  public identity
-  public token
-  public errorMessage
-  public alertRegister: string
-  public url: string
 
-  constructor(private _userService: UserService) {
-    this.user = new User('', '', '', '', '', 'ROLE_USER', '')
-    this.userRegister = new User('', '', '', '', '', 'ROLE_USER', '')
+export class AppComponent {
+  public title = 'hs-music';
+  public user:User;
+  public userRegister: User
+  public identity:String;
+  public token:String;
+  public errorMessage:String
+  public alertRegister: String
+  public url: String
+
+  constructor(){
+    this.user = new User('', '', '', '', '', 'ROLE_USER', '');
+    this.userRegister = new User('', '', '', '', '', 'ROLE_USER', '');
     this.url = GLOBAL.url
   }
 
-  ngOnInit()
-  {
-    this.identity = this._userService.getIdentity()
-    this.token = this._userService.getToken()
+  ngOnInit() {
+    console.log('INIT COMPONENT');
   }
 
-  public onSubmit()
-  {
-    // GET DATA USER IDENTIFIED
-    this._userService.signup(this.user).subscribe(
-      response => {
-        let identity = response.user
-        this.identity = identity
-
-        if (!this.identity._id) {
-          return alert('El usuario no esta correctamente identificado')
-        }
-        //CREATE ELEMENT LOCALSTORTAGE
-        localStorage.setItem('identity', JSON.stringify(identity))
-        // BUILD TOKEN
-        this._userService.signup(this.user, 'true').subscribe(
-          response => {
-            let token = response.token
-            this.token = token
-
-            if (this.token <= 0) {
-              return alert('El Token no se ha generado')
-            }
-            //CREATE TOKEN IN LOCALSTORAGE
-            localStorage.setItem('token', token)
-            this.user = new User('', '', '', '', '', 'ROLE_USER', '')
-          },
-          error => {
-            var ErrorMessage = <any>error
-            if (ErrorMessage != null) {
-              var body = JSON.parse(error._body)
-              this.errorMessage = body.message
-            }
-          })
-
-      },
-      error => {
-        var ErrorMessage = <any>error
-        if (ErrorMessage != null) {
-          var body = JSON.parse(error._body)
-          this.errorMessage = body.message
-        }
-      })
+  public onSubmit(){
+    console.log('SUBMIT');
   }
-  /**
-   * Close session
-   * @return void
-  */
-  public logout()
-  {
+
+  public logout(){
+    console.log('LOGOUT');
     localStorage.removeItem('identity')
     localStorage.removeItem('token')
     localStorage.clear()
     this.identity = null
     this.token = null
   }
-  /**
-   * Register User
-   * @return void
-  */
-  public onSubmitRegister()
-  {
-    console.log(this.userRegister)
-    this._userService.register(this.userRegister).subscribe(
-      response => {
-        let user = response.user
-        this.userRegister = user
 
-        if (!user._id) {
-          this.alertRegister = 'Error al registrarse'
-        } else {
-          this.alertRegister = 'El registro se ha realizado correctamente, Registrate con: ' + this.userRegister.email
-          this.userRegister = new User('', '', '', '', '', 'ROLE_USER', '')
-        }
-      },
-      error => {
-        var ErrorMessage = <any>error
-        if (ErrorMessage != null) {
-          var body = JSON.parse(error._body)
-          this.alertRegister = body.message
-          console.log(error)
-        }
-      }
-    )
+  public onSubmitRegister() {
+    console.log(this.userRegister);
   }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,43 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { routing, appRoutingProviders } from './app.routing'
 
-import { HomeComponent } from "./components/home.component";
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserEditComponent } from './components/userEdit';
-import { ArtistListComponent } from './components/artistList.component';
-import { ArtistAddComponent } from './components/artistAdd.component';
-import { ArtistEditComponent } from './components/artistEdit.component';
-import { ArtistDetailComponent } from './components/artistDetail.component';
-import { AlbumAddComponent } from './components/albumAdd.component';
-import { AlbumEditComponent } from './components/albumEdit.component';
-import { AlbumDetailComponent } from './components/albumDetail.component';
-import { SongAddComponent } from './components/songAdd.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    UserEditComponent,
-    ArtistListComponent,
-    ArtistAddComponent,
-    ArtistEditComponent,
-    ArtistDetailComponent,
-    AlbumAddComponent,
-    AlbumEditComponent,
-    AlbumDetailComponent,
-    SongAddComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule,
-    routing
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [appRoutingProviders],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule{}
+export class AppModule { }
